cbee54a7328ce597fc60af1c1f185a27a797d859
willantunes <antunes406@gmail.com>
2026-06-21 20:16:13 +0200

    chore: ajusta base Fresh 2 e Vite
    iFactory plan f22a6fac-964d-45ea-9e7c-d5f299618918

--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,7 @@ import { fresh } from "@fresh/plugin-vite";
 
 export default defineConfig({
   plugins: [fresh()],
+  ssr: {
+    noExternal: ["$fresh", "$fresh/", "preact", "preact-render-to-string"],
+  },
 });
